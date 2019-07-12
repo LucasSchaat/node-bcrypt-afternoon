037f650ecd3e7121c7dec673fa0c3ad6d21a4760
Lucas <lucas@Ashleys-MacBook-Pro.local>
2019-07-11 23:00:54 -0600

    Afternoon Project Submission

--- server/controllers/treasureController.js
+++ server/controllers/treasureController.js
@@ -8,5 +8,17 @@ module.exports = {
         let db = req.app.get('db')
         let userTreasure = await db.get_user_treasure(req.session.user.id)
         return res.status(200).send(userTreasure)
+    },
+    addUserTreasure: async (req, res) => {
+        const { treasureURL } = req.body
+        const { id } = req.session.user
+        let db = req.app.get('db')
+        let userTreasure = await db.add_user_treasure([treasureURL, id])
+        return res.status(200).send(userTreasure)
+    },
+    getAllTreasure: async (req, res) => {
+        let db = req.app.get('db')
+        let allTreasure = await db.get_all_treasure()
+        res.status(200).send(allTreasure)
     }
 }
--- server/index.js
+++ server/index.js
@@ -34,6 +34,8 @@ app.post('/auth/login', authCtrl.login)
 app.get('/auth/logout', authCtrl.logout)
 app.get('/api/treasure/dragon', treasureCtrl.dragonTreasure)
 app.get('/api/treasure/user', auth.usersOnly, treasureCtrl.getUserTreasure)
+app.post('/api/treasure/user', auth.usersOnly, treasureCtrl.addUserTreasure)
+app.get('/api/treasure/all', auth.usersOnly, auth.adminsOnly, treasureCtrl.getAllTreasure)
 
 app.listen(PORT, () => {
     console.log(`Listening on Port ${PORT}`)
